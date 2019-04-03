b0dd34ab808b407174d3a060c19711dd0e320d3a
Fil_work <f.nikiforov@gmail.com>
2019-04-03 10:47:04 +0300

    map

--- src/app/photo-reports/photo-reports.component.ts
+++ src/app/photo-reports/photo-reports.component.ts
@@ -8,20 +8,23 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./photo-reports.component.scss']
 })
 export class PhotoReportsComponent implements OnInit {
-
+test:any;
   id:any;
 url:any;
 response:any;
 elements:any;
+htmlToAdd:any;
+
   constructor(private routes: ActivatedRoute, private http:HttpClient
     ) {
 
       this.url="http://net/api/getxml.php";
-  
+ 
       this.http.get(this.url).subscribe((response)=>{
        this.response=response;
        this.elements=this.response.object;
-       console.log(this.elements);
+       console.log(this.elements[0].bndbox.xmin);
+      
     
      
      })
